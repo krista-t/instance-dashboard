18dd01684ea7db26cefd399ea857757fc78b198f
krista-t <krista.tomicic@gmail.com>
2021-09-13 17:57:29 +0200

    header component and style done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
+import Header from "./Components/Header";
 function App() {
   return (
     <div className="App">
-   
+   <Header></Header>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+    return (
+        <header className = "header-container">
+            <div className="flex-container">
+            <div className="logo">LOGO</div>
+            <nav className = "navigation">
+                <ul>
+                    <li>LINK</li>
+                    <li>LINK</li>
+                    <li>LINK</li>
+                </ul>
+            </nav>
+            </div>   
+        </header>
+    )
+}
+
+export default Header
