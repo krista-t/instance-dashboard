ea7701de3582ee4214df4acc72a5bd3b6e946aa5
krista-t <krista.tomicic@gmail.com>
2021-09-13 20:49:18 +0200

    footer components and style added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Header from "./Components/Header";
 import Template from "./Components/Template";
 import Search from "./Components/Search";
+import Footer from "./Components/Footer";
 function App() {
   const API_templates = "https://data.windenergy.dtu.dk/api/sesame/v1/get-template-ids"
   const [templates, setTemplates]=useState([]);
@@ -26,6 +27,7 @@ function App() {
    <Template templates = {templates} 
    ></Template>
    <Search></Search>
+   <Footer></Footer>
     </div>
   );
 }
