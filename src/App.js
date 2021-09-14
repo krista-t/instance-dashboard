298c352e938cf589547b6974e592168ba5f41b45
krista-t <krista.tomicic@gmail.com>
2021-09-14 18:32:35 +0200

    fetch facets api

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       try {
         const response = await fetch(`${API_templates}`);
         const data = await response.json();
+        console.log(data)
     setTemplates(data)
       
       } catch (err) {
@@ -21,12 +22,27 @@ function App() {
     fetchItems();
     
   }, []);
-
+  const API_facets = "https://data.windenergy.dtu.dk/api/sesame/v1/get-facets?templateID= "
+  //SET DEFAULT STATE FOR FACETS REQ
+  const [reqType, setReqType] = useState("https%3A%2F%2Frepo.metadatacenter.org%2Ftemplates%2Fc2f771b8-7cd6-445d-8706-f3d8b9ae3134");
+  //CHANGE FACET STATE STATE DEPENDING ON TEMPLATE ID
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_facets}${reqType}`);
+        const data = await response.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchItems();
+  }, [reqType]); 
+  //console.log(reqType);
   return (
     <div className="App">
    <Header></Header>
-   <Template templates = {templates} 
-   ></Template>
+   <Template templates = {templates} reqType = {reqType} setReqType = {setReqType}></Template>
    <Search></Search>
    <Facets></Facets>
    <Footer></Footer>
