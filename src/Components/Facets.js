9a585d5009a07f9749c8adcdb992a9f85ab22243
krista-t <krista.tomicic@gmail.com>
2021-09-16 10:25:00 +0200

    conditional render checkboxes

--- src/Components/Facets.js
+++ src/Components/Facets.js
@@ -2,7 +2,7 @@ import { useState} from "react";
 import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
 
 const Facets = ({facets, facetID}) => {
-    console.log(facets)
+    
     //DO NOT SHOW CHECKBOXES ON INITIAL RENDER
     const [showBoxes, setShowBoxes]= useState(true);
 const toggle =()=> setShowBoxes(!showBoxes);
@@ -11,33 +11,27 @@ const toggle =()=> setShowBoxes(!showBoxes);
             <div className="facets-container">
                 <form>
                 <ul className="facets-title-container"> 
-                
                 {Object.keys(facets).map((key,i) => 
-                
-              (<li key={i} value={key}  onClick={()=> toggle(!showBoxes)}
-                >{key}<span className = "icon">{!showBoxes ?<FaAngleUp/>:<FaAngleDown/>}</span> 
-                
-                 </li>)
-            
-                )}
-                
-                     </ul> <br/>
-
-                     {/* TODO: dynamically display */}
-               {/* {!showBoxes ? (<>
-                <input type="checkbox"
-                 name = "language" value = "eng" />
-                  <label for= "language">eng</label><br/>
-                <input type="checkbox"
-                 name = "language" value = "eng" />
-                  <label for= "language">eng</label><br/>
-                <input type="checkbox"
-                 name = "language" value = "eng" />
-                  <label for= "language">eng</label><br/>
-
-                  </>): null}  */}
+                  (<li key={i} value={key}  onClick={()=> toggle(!showBoxes)}
+                   >{key}<span className = "icon">{!showBoxes ?<FaAngleUp/>:<FaAngleDown/>}</span> <br/>
+  {/* TODO: consditionally render */}
+                    {Object.keys(facets[key]).map((item,i)=>
+                        {let boxVal = facets[key][item]["rdfs:label"]
+                        console.log(boxVal)
+                         return(
+                           <div>
+                 {!showBoxes ? (<>
+                <input type="checkbox" key ={i}
+                 name = {boxVal} value = {boxVal} />
+                  <label for= {boxVal}>{boxVal}</label><br/>
+                  </>): null} 
+                          </div>)} 
+                    )}   
+                  </li>) 
+                 )}
+                </ul>
                 </form>
-                </div>
+          </div>
         </section>
     )
 }
